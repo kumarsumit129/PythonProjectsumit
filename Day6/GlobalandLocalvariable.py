966646df015d9db22bc4d777a858cc194f9f028b
kumarsumit129 <sumitkumargavaskar@gmail.com>
2025-02-13 21:18:03 +0530

    day 6th updated

---
+++ Day6/GlobalandLocalvariable.py
@@ -0,0 +1,18 @@
+
+#Example1
+# global_var=20 # global variable
+#
+# def func():
+#
+local_var=10 # local variable
+#
+print (local_var)
+#
+print (global_var)
+#
+# func()
+#
+# #print (local_var) # invalid bcoz local_var is local varaible of func()
+# print(global_var) # valid bcoz global_var is global variable
+
+#(Example2)
---
+++ Day6/functions.py
@@ -0,0 +1,25 @@
+#Example1
+
+# def myfun():
+#     print('hello')  #function defination
+# myfun() #call the function
+
+#Example2
+# def myfun(name):
+#     print("hello", name)
+# myfun('smith') #hello smith
+
+#Examole3
+# def cal(a, b):
+#     return (a+b)
+# sum=cal(100,200)
+# print(sum) #300
+# print(cal(10,20)) #30
+
+#Examole4
+def cal(a, b):
+    print(a+b)
+cal(10, 20)
+def cal(a, b):
+    return (a+b)
+print(cal(10,20))
